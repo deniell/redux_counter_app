431676d70fe95c4306720be95d3ed47504b0281c
Denis Stahovsky <denis.stahovsky@vantino.ch>
2023-09-24 21:37:50 +0300

    - reducer separated on _counterReducer and _textReducer.

--- lib/core/reducers.dart
+++ lib/core/reducers.dart
@@ -1,16 +1,25 @@
 import 'package:redux_counter_app/core/actions.dart';
 import 'package:redux_counter_app/core/app_state.dart';
 
-AppState reducer(AppState state, dynamic action) {
+AppState reducer(AppState state, dynamic action) => AppState(
+    counter: _counterReducer(state, action), text: _textReducer(state, action));
+
+int _counterReducer(AppState state, dynamic action) {
   if (action is AddAction) {
-    return AppState(counter: state.counter + 1, text: state.text);
+    return state.counter + 1;
   } else if (action is RemoveAction) {
-    return AppState(counter: state.counter - 1, text: state.text);
-  } else if (action is SetTextAction) {
-    return AppState(counter: state.counter, text: action.text);
-  } else if (action is ResetTextAction) {
-    return AppState(counter: state.counter, text: "");
+    return state.counter - 1;
   } else {
-    return state;
+    return state.counter;
+  }
+}
+
+String _textReducer(AppState state, dynamic action) {
+  if (action is SetTextAction) {
+    return action.text;
+  } else if (action is ResetTextAction) {
+    return "";
+  } else {
+    return state.text;
   }
 }

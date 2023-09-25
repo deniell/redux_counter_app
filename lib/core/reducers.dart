b76e3fdcb6122b267d0303f35e1781c77673c9a0
Denis Stahovsky <denis.stahovsky@vantino.ch>
2023-09-25 23:12:02 +0300

    - added "betterReducer" based on combineReducers - utility function that combines several reducers.

--- lib/core/reducers.dart
+++ lib/core/reducers.dart
@@ -1,6 +1,30 @@
+import 'package:redux/redux.dart';
 import 'package:redux_counter_app/core/actions.dart';
 import 'package:redux_counter_app/core/app_state.dart';
 
+// implementation of reducer with use of combinedReducers
+AppState betterReducer(AppState state, dynamic action) => AppState(
+    counter: _betterCounterReducer(state.counter, action),
+    text: _betterTextReducer(state.text, action),
+);
+
+int _addCounterReducer(int count, AddAction action) => count + 1;
+int _removeCounterReducer(int count, RemoveAction action) => count - 1;
+
+Reducer<int> _betterCounterReducer = combineReducers([
+  TypedReducer<int, AddAction>(_addCounterReducer),
+  TypedReducer<int, RemoveAction>(_removeCounterReducer),
+]);
+
+String _setTextReducer(String text, SetTextAction action) => action.text;
+String _resetTextReducer(String text, ResetTextAction action) => "";
+
+Reducer<String> _betterTextReducer = combineReducers([
+  TypedReducer<String, SetTextAction>(_setTextReducer),
+  TypedReducer<String, ResetTextAction>(_resetTextReducer),
+]);
+
+// simple reducer implementation
 AppState reducer(AppState state, dynamic action) => AppState(
     counter: _counterReducer(state, action), text: _textReducer(state, action));
 
--- lib/core/stores.dart
+++ lib/core/stores.dart
@@ -3,7 +3,7 @@ import 'package:redux_counter_app/core/app_state.dart';
 import 'package:redux_counter_app/core/reducers.dart';
 
 final Store<AppState> store = Store(
-  reducer,
+  betterReducer,
   initialState: const AppState(
     counter: 0,
     text: "Your text will be set here"
